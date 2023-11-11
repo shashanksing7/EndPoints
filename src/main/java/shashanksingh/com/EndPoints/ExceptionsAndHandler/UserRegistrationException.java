7757a8689e610e5979425bfcdd670388d445d645
shashank singh <68452635+shashanksing7@users.noreply.github.com>
2023-11-11 13:06:25 +0545

    Delete src directory

--- src/main/java/shashanksingh/com/EndPoints/ExceptionsAndHandler/UserRegistrationException.java
+++
@@ -1,9 +0,0 @@
-package shashanksingh.com.EndPoints.ExceptionsAndHandler;
-/*
-This exception will be occurred when the user is already registered with that username.
- */
-public class UserRegistrationException extends  RuntimeException{
-    public UserRegistrationException(String Message){
-        super(Message);
-    }
-}
